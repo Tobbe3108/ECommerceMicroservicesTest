ceac870c04887ad1a035b27ec5032022aa7207db
Tobbe3108 <Tobbe3108@gmail.com>
2020-03-31 23:07:13 +0200

    docker compose update

--- ECommerce.Api.Orders/Db/Order.cs
+++ ECommerce.Api.Orders/Db/Order.cs
@@ -1,5 +1,4 @@
-﻿using ECommerce.Api.Products.Db;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
--- ECommerce.Api.Orders/Models/Order.cs
+++ ECommerce.Api.Orders/Models/Order.cs
@@ -1,5 +1,4 @@
-﻿using ECommerce.Api.Products.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
